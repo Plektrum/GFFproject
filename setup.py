8370d77fde7ca0eb4a7f017829590d32138cb976
viktoria <viktoria12345@gmail.com>
2013-07-09 19:21:09 +0300

    change setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,12 @@ Created on 8 Jul 2013
 
 @author: Plektrum
 '''
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='src',
+    version=src.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+)
