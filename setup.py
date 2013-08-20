80f77b392d15cd13c0ecdb3f99550317e7e690e6
Viktorija <viktoria12345@gmail.com>
2013-08-20 17:30:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 '''
-Created on 8 Jul 2013
-
 @author: Plektrum
 '''
+#setup py
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
     name='GffReader',
-    version=GffReader.__version__,
+    version='1.0',
+    scripts=['GffReader.py']
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
